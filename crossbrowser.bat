7e96c0695f2181d436697a9bcc4ccd587088de56
DuotechB9 <duotechb9@gmail.com>
2022-10-27 20:04:47 -0400

    comments added

--- crossbrowser.bat
+++ crossbrowser.bat
@@ -1,3 +1,5 @@
 call mvn test -D xmlFile=smoke-tests.xml -D browser=chrome
 call mvn test -D xmlFile=smoke-tests.xml -D browser=firefox
 call mvn test -D xmlFile=smoke-tests.xml -D browser=edge
+
+:: run this file thru cmd using the name of the file -> crossbrowser.bat
--- crossbrowserMac.sh
+++ crossbrowserMac.sh
@@ -2,3 +2,5 @@
 mvn test -D xmlFile=smoke-tests.xml -D browser=chrome
 mvn test -D xmlFile=smoke-tests.xml -D browser=firefox
 mvn test -D xmlFile=smoke-tests.xml -D browser=edge
+
+#run this file in Mac/Linux OS with ./crossbrowserMac.sh command
